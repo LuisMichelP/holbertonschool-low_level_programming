26bd5e969eaece402605ce69391351aa0ab426b2
root <luismichelpcontacto@gmail.com>
2025-02-14 16:35:42 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/* Print with put for c */
+/* Print with put for c in the main function (In case you read this, I was testing the platform*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
