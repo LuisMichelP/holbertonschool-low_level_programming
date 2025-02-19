02d1519dbd146ad35b75e5be9763bf12709a28cc
root <luismichelpcontacto@gmail.com>
2025-02-19 04:25:57 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - print alphabet
  * Function that prints the alphabet
  * with putchar
  * Return: 0 if success
  */
-
 int main(void)
 {
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		if (alphabet != 'q' && alphabet != 'e')	    
-			putchar(alphabet);	 
+		if (alphabet != 'q' && alphabet != 'e')
+			putchar(alphabet);
 	putchar('\n');
 	return (0);
 }
