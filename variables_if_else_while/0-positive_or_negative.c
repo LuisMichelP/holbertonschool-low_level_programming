1006d6216dac1e2d916730ce21a6f3ec9a66793f
root <luismichelpcontacto@gmail.com>
2025-02-19 02:49:39 +0000

    if prints

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.io>
+/*
+ * main - print if's
+ * function to print depending on the case
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is positive\n", n);
+	return (0);
+}
