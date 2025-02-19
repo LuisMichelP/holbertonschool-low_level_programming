d432ca5fc4534ccf22ee5f9dd8c8b5a528e07615
root <luismichelpcontacto@gmail.com>
2025-02-19 04:42:11 +0000

    ez?

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Function that prints the alphabet
+ * with putchar
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = '0'; alphabet <= 'f'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
