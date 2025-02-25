fa3ff4033b2cdf26787fb05f8573037c9d0bf4ea
root <luismichelpcontacto@gmail.com>
2025-02-25 04:14:25 +0000

    hi

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
+ * main - print function
+ * function to print
+ * Return: 0 if success
  */
 void print_alphabet(void)
 {
 	int a;
 	for (a = 0; a<26; a++)
-		_putchar(a);
+		_putchar(97+a);
 	_putchar('\n');
 }
