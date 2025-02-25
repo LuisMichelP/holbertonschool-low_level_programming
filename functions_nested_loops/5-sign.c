381aa99c32c9d909ee9946d068feb01278af9445
root <luismichelpcontacto@gmail.com>
2025-02-25 05:07:15 +0000

    Sway away

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - print the sign of the number
+ * @n: int
+ * Prints the sign
+ * Return: 1 if greater than 0 and viceversa
+ */
+
+int prin_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return(0);
+	}
+}
