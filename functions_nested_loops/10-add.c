d214eb49bc17da7332e72c05930d672ca23f8ac0
root <luismichelpcontacto@gmail.com>
2025-02-26 05:03:24 +0000

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - addition function
+ * @a: int
+ * @b: int
+ * Return: int result of the function
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
