cc8b9422fc14a12b86393b8a05f972259d1dade2
root <luismichelpcontacto@gmail.com>
2025-02-26 04:45:53 +0000

    veracruz

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,19 +18,19 @@ void times_table(void)
 		_putchar('0');
 		for (x = 1; x <= 9; x++)
 			{
-				_putchar(',');
+			_putchar(',');
+			_putchar(32);
+			r = i * x;
+			if (r > 9)
+			{
+				_putchar('0' + (r / 10));
+				_putchar('0' + (r % 10));
+			}
+			else
+			{
 				_putchar(32);
-				r = i * x;
-				if (r > 9)
-				{
-					_putchar('0' + (r / 10));
-					_putchar('0' + (r % 10));
-				}
-				else
-				{
-					_putchar(32);
-					_putchar('0' + (i * x));
-				}
+				_putchar('0' + (i * x));
+			}
 			}
 		_putchar('\n');
 		}
