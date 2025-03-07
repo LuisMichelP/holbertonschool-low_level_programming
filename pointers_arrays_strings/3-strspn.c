a6328bf2cefd5cf6369df9a80983a8fa397e20b1
root <luismichelpcontacto@gmail.com>
2025-03-07 05:20:18 +0000

    changes

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,17 +14,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	int i, j;
 	int c = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		if (s[i] != ' ')
 		{
-			if (s[i] == accept[j])
-				c++;
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
 		}
+		else
+			return (c);
 	}
-
 	return (c);
 }
