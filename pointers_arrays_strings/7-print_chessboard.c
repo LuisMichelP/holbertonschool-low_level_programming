6399b0225bcbd92093f3751654de0d0f7c9e6817
root <luismichelpcontacto@gmail.com>
2025-03-07 04:58:19 +0000

    hundred

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -15,9 +15,9 @@ void print_chessboard(char (*a)[8])
 {
 	unsigned int i, j;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j <= 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			putchar(a[i][j]);
 		}
