74199e38edb0fa59060859341dac3766be9a4e6a
root <luismichelpcontacto@gmail.com>
2025-03-07 00:58:10 +0000

    memory set 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset: function to fill memory in a string
+ * Just for fun
+ * Return: str s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0, i < n, i++)
+        s[i] = b;
+
+    return (s);
+}
