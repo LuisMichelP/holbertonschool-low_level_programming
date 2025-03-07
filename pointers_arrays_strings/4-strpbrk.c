3e98d42e3e251c79f0196e44e656026b7ea5c7aa
root <luismichelpcontacto@gmail.com>
2025-03-07 04:24:11 +0000

    zizek

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -15,14 +15,13 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
-	int c = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
-				return (*s[i]);
+				return (s[i]);
 		}
 	}
 
