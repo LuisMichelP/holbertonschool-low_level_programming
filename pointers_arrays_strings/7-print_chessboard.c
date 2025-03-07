499efbbb4567fbad95ce445ae9b71177ab86fdf1
root <luismichelpcontacto@gmail.com>
2025-03-07 04:54:43 +0000

    movement

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - function
+ *
+ * print the chessboard they gave
+ *
+ * @a: array chessboard
+ *
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			_putchar(a[i][i])
+		}
+		_putchar('\n')
+	}
+}
