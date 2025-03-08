4273476bbdbf42e3788a57182b8b921cc7f1a47b
root <luismichelpcontacto@gmail.com>
2025-03-08 22:22:11 +0000

    Metro balderas

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -15,12 +15,12 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
 	else
 	{
-		putchar(*s);
+		_putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}
