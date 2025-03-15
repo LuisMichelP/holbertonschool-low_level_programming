643349d3c2e9d045363a36b35dfb57595bb47ada
root <luismichelpcontacto@gmail.com>
2025-03-15 19:33:33 +0000

    A horse with no name

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * str_concat - function
+ *
+ * Function to concat 2 strings
+ *
+ * @s1: str
+ * @s2: str
+ *
+ * Return: rep
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *rep;
+	unsigned int i, len1 = 0, len2 = 0;
+
+	if ((s1 == NULL) || (s2 == NULL))
+		return (NULL);
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
+
+	rep = malloc(sizeof(char) * len1 * len2);
+
+	if (rep == NULL)
+		return (NULL)
+
+	for (i = 0; i < len1; i++)
+	{
+		rep[i] = s1[i];
+	}
+	for (i = 0; i < len2; i++)
+	{
+		rep[len1 + i] = s2[i];
+	}
+
+	return (rep)
+}
