8b21c43dad5948c614a4bf0254dbbaa7d8db5b6c
root <luismichelpcontacto@gmail.com>
2025-03-15 19:17:24 +0000

    free

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * _strdup - function
+ * _strdup - Returns a pointer to a new string which is a duplicate of str
+ * @str: The string to duplicate
  *
- * @str: string
- *
- * Return: t
+ * Return: Pointer to the duplicated string, or NULL if memory allocation fails
  */
-
 char *_strdup(char *str)
 {
-	char *t;
-	unsigned int i;
+    char *t;
+    unsigned int i, length = 0;
 
-	if (str == NULL)
-		return (NULL);
-	
-	for (i = 0; (str[i] <= '\0'); i++)
-	{
-		t = malloc(sizeof(char) * i);
-		if (t == NULL)
-			return (NULL);
-		t[i] = str[i];
-		i++;
-	}
-		
-	return (t);
+    if (str == NULL)
+        return (NULL);
+
+    /* Obtener la longitud de la cadena */
+    while (str[length] != '\0')
+        length++;
+
+    /* Asignar memoria (incluye espacio para el terminador nulo) */
+    t = malloc(sizeof(char) * (length + 1));
+    if (t == NULL)
+        return (NULL);
+
+    /* Copiar la cadena */
+    for (i = 0; i < length; i++)
+        t[i] = str[i];
+
+    /* Agregar el terminador nulo */
+    t[length] = '\0';
+
+    return (t);
 }
