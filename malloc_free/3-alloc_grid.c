2ffd59db9969d9f57e922cae06fcdb27441ad839
root <luismichelpcontacto@gmail.com>
2025-03-16 04:46:22 +0000

    breakfast

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -30,12 +30,12 @@ int **alloc_grid(int width, int height)
 	{
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
-        {
-            while (i-- > 0)
-                free(arr[i]);
-            free(arr);
-            return (NULL);
-        }
+		{
+			while (i-- > 0)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
