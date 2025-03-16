89dd668e58e588262a3c9a6cf5dcd2d84885e3ef
root <luismichelpcontacto@gmail.com>
2025-03-16 03:15:20 +0000

    pure_immagination

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,6 +18,11 @@ char *str_concat(char *s1, char *s2)
 	char *rep;
 	unsigned int i, len1 = 0, len2 = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len1] != '\0')
 		len1++;
 
