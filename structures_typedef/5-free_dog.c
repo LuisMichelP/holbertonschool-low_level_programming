ca75aacf459aa163943709c941d97b3dbd51b3c0
root <luismichelpcontacto@gmail.com>
2025-03-17 01:40:18 +0000

    jsjs

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,44 +1,18 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Return: A pointer to the new dog_t structure, or NULL if it fails.
- */
-dog_t *new_dog(char *name, float age, char *owner)
+  * free_dog - ...
+  * @d: ...
+  *
+  * Return: ...
+  */
+void free_dog(dog_t *d)
 {
-    dog_t *dog;
-    
-    if (name == NULL || owner == NULL)
-        return (NULL);
-
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
-
-    dog->name = malloc(strlen(name) + 1);
-    if (dog->name == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
-    strcpy(dog->name, name);
-
-    dog->owner = malloc(strlen(owner) + 1);
-    if (dog->owner == NULL)
-    {
-        free(dog->name);
-        free(dog);
-        return (NULL);
-    }
-    strcpy(dog->owner, owner);
-
-    dog->age = age;
-
-    return (dog);
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
