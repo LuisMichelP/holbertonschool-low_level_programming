2bf16eec770df6889fbfc1404704e928a5f5e657
root <luismichelpcontacto@gmail.com>
2025-03-17 00:56:23 +0000

    FreeMalloc

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - a 2 dimensional array of integers
+ *
+ * @width: an integr
+ * @height: an integer
+ *
+ * Return: a pointer to a 2 dimensional array of integers
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **t;
+
+	if ((width <= 0) || (height <= 0))
+		return (NULL);
+	t = malloc(sizeof(int *) * height);
+	if (t == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		t[i] = malloc(sizeof(int) * width);
+		if (t[i] == NULL)
+		{
+			for (--i; i >= 0; i--)
+				free(t[i]);
+			free(t);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			t[i][j] = 0;
+	return (t);
+}
